0c129ecaaa09bf081b881f53f9865dd2c10649e6
Maria Cecília Corrêa <cicinha.correa@gmail.com>
2022-04-05 21:57:13 -0300

    begin e5

---
+++ e5/ilocgen.cpp
@@ -0,0 +1,6 @@
+#include "ilocgen.h"
+
+int reg_counter = 0;
+int label_counter = 0;
+
+void generate_iloc(struct node* root){}
---
+++ e5/ilocgen.h
@@ -0,0 +1,3 @@
+#include <bits/stdc++.h>
+
+void generate_iloc(struct node* root);
